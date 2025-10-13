7075ec2b9b73f5539442399ec39a835f7ca6eca8
da237 <daniel951931@outlook.es>
2025-10-12 19:24:22 -0500

    Feat:Se realiza la configuracion del imput para garegar atareas y la funcion para agregarlas

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,44 +1,39 @@
 <script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true,
-  },
-})
+import {ref,onMounted } from 'vue';
+
+const imput = ref('');
+
+const listTasks = ref([]);
+
+const addTask = () => {
+  listTasks.value.push({
+    name: imput.value,
+    date: new Date().toLocaleDateString()
+  });
+  imput.value = '';
+  console.log(listTasks.value);
+}
+
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+  <div>
+    <label for="">Ingrese tarea</label>
+    <input type="text" v-model="imput">
+
+    <button @click="addTask">Agregar Tarea</button>
+
+
+
+
+    <ul>
+      <li v-for="(task,index) in listTasks" :key="index">
+        {{task.name}} - {{task.date}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
 
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
 </style>
